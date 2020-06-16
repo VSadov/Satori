025d124d623cbda77e53505c482e3c221eec670c
vsadov <vsadov@microsoft.com>
2020-06-16 15:46:15 -0700

    SatoriObject + some cleanup

--- src/coreclr/src/gc/satori/SatoriAllocationContext.h
+++ src/coreclr/src/gc/satori/SatoriAllocationContext.h
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 //
-// SatoriGCHeap.h
+// SatoriAllocationContext.h
 //
 
 #ifndef __SATORI_ALLOCATION_CONTEXT_H__
