70e01a149c592242524e1cd63a911bb4ee0624f4
vsadov <vsadov@microsoft.com>
2020-06-16 15:46:15 -0700

    SatoriObject + some cleanup

--- src/coreclr/src/gc/satori/SatoriRecycler.h
+++ src/coreclr/src/gc/satori/SatoriRecycler.h
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 //
-// SatoriGCHeap.h
+// SatoriRecycler.h
 //
 
 #ifndef __SATORI_RECYCLER_H__
