69b403e42a7c9ee235140cbd1ec60e199dad0f4b
vsadov <vsadov@microsoft.com>
2020-06-16 15:46:15 -0700

    SatoriObject + some cleanup

--- src/coreclr/src/gc/satori/SatoriRegionQueue.h
+++ src/coreclr/src/gc/satori/SatoriRegionQueue.h
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 //
-// SatoriGCHeap.h
+// SatoriRegionQueue.h
 //
 
 #ifndef __SATORI_REGION_QUEUE_H__
