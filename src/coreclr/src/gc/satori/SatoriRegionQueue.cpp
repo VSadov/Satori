cbeca8c461c7d1583d2e0dfad05fcc356119bec2
vsadov <vsadov@microsoft.com>
2020-07-31 12:07:12 -0700

    SatoriQueue

--- src/coreclr/src/gc/satori/SatoriRegionQueue.cpp
+++ src/coreclr/src/gc/satori/SatoriRegionQueue.cpp
@@ -15,74 +15,6 @@
 #include "SatoriRegion.h"
 #include "SatoriRegion.inl"
 
-void SatoriRegionQueue::Push(SatoriRegion* region)
-{
-    _ASSERTE(region->ValidateBlank());
-
-    SatoriLockHolder<SatoriLock> holder(&m_lock);
-    region->m_containingQueue = this;
-    if (m_head == nullptr)
-    {
-        _ASSERTE(m_tail == nullptr);
-        m_head = m_tail = region;
-    }
-    else
-    {
-        region->m_next = m_head;
-        m_head->m_prev = region;
-        m_head = region;
-    }
-}
-
-void SatoriRegionQueue::Enqueue(SatoriRegion* region)
-{
-    _ASSERTE(region->ValidateBlank());
-
-    SatoriLockHolder<SatoriLock> holder(&m_lock);
-    region->m_containingQueue = this;
-    if (m_tail == nullptr)
-    {
-        _ASSERTE(m_head == nullptr);
-        m_head = m_tail = region;
-    }
-    else
-    {
-        region->m_prev = m_tail;
-        m_tail->m_next = region;
-        m_tail = region;
-    }
-}
-
-SatoriRegion* SatoriRegionQueue::TryPop()
-{
-    SatoriRegion* result;
-    {
-        SatoriLockHolder<SatoriLock> holder(&m_lock);
-        result = m_head;
-        if (result == nullptr)
-        {
-            return nullptr;
-        }
-
-        result->m_containingQueue = nullptr;
-        m_head = result->m_next;
-        if (m_head == nullptr)
-        {
-            m_tail = nullptr;
-        }
-        else
-        {
-            m_head->m_prev = nullptr;
-        }
-    }
-
-    _ASSERTE(result->m_prev == nullptr);
-    result->m_next = nullptr;
-
-    _ASSERTE(result->ValidateBlank());
-    return result;
-}
-
 SatoriRegion* SatoriRegionQueue::TryPop(size_t regionSize, SatoriRegion*& putBack)
 {
     m_lock.Enter();
@@ -217,43 +149,4 @@ SatoriRegion* SatoriRegionQueue::TryRemove(size_t regionSize, SatoriRegion*& put
     }
 
     return result;
-}
-
-bool SatoriRegionQueue::Contains(SatoriRegion* region)
-{
-    return region->m_containingQueue == this;
-}
-
-bool SatoriRegionQueue::TryRemove(SatoriRegion* region)
-{
-    {
-        SatoriLockHolder<SatoriLock> holder(&m_lock);
-        if (!Contains(region))
-        {
-            return false;
-        }
-
-        region->m_containingQueue = nullptr;
-        if (region->m_prev == nullptr)
-        {
-            m_head = region->m_next;
-        }
-        else
-        {
-            region->m_prev->m_next = region->m_next;
-        }
-
-        if (region->m_next == nullptr)
-        {
-            m_tail = region->m_prev;
-        }
-        else
-        {
-            region->m_next->m_prev = region->m_prev;
-        }
-    }
-
-    region->m_next = nullptr;
-    region->m_prev = nullptr;
-    return true;
 }
