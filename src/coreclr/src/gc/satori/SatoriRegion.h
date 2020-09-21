b86f7179f105ebbb2ab033f26c7ec4fb2925549c
vsadov <vsadov@microsoft.com>
2020-09-21 11:41:13 -0700

    few fixes

--- src/coreclr/src/gc/satori/SatoriRegion.h
+++ src/coreclr/src/gc/satori/SatoriRegion.h
@@ -84,10 +84,10 @@ public:
     void Verify(bool allowMarked = false);
 
 private:
-    static const int BITMAP_SIZE = Satori::REGION_SIZE_GRANULARITY / sizeof(size_t) / sizeof(size_t) / 8;
+    static const int BITMAP_LENGTH = Satori::REGION_SIZE_GRANULARITY / sizeof(size_t) / sizeof(size_t) / 8;
 
     // The first actually useful index is offsetof(m_firstObject) / sizeof(size_t) / 8,
-    static const int BITMAP_START = (BITMAP_SIZE + Satori::INDEX_ITEMS) / sizeof(size_t) / 8;
+    static const int BITMAP_START = (BITMAP_LENGTH + Satori::INDEX_LENGTH) / sizeof(size_t) / 8;
     union
     {
         // object metadata - one bit per size_t
@@ -97,7 +97,7 @@ private:
         // we will overlap the map and the header for simplicity of map operations.
         // it is ok because the first BITMAP_START elements of the map cover the header/map and thus will not be used.
         // +1 to include End(), it will always be 0, but it is conveninet to make it legal map index.
-        size_t m_bitmap[BITMAP_SIZE + 1];
+        size_t m_bitmap[BITMAP_LENGTH + 1];
 
         // Header.(can be up to 72 size_t)
         struct
@@ -132,7 +132,7 @@ private:
         };
     };
 
-    SatoriObject* m_index[Satori::INDEX_ITEMS];
+    SatoriObject* m_index[Satori::INDEX_LENGTH + 1];
 
     size_t m_syncBlock;
     SatoriObject m_firstObject;
