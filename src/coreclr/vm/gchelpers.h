28231741cfa47bc20fe63b17fdf1248d4f2c54a4
vsadov <vsadov@microsoft.com>
2020-09-25 12:15:35 -0700

    added gen num and ForEachObjectRef with range, ForEachPage, barrier, card size and initialization fixes

--- src/coreclr/vm/gchelpers.h
+++ src/coreclr/vm/gchelpers.h
@@ -68,7 +68,7 @@ extern void ThrowOutOfMemoryDimensionsExceeded();
 void ErectWriteBarrier(OBJECTREF* dst, OBJECTREF ref);
 bool IsInHeapSatori(void* ptr);
 void CheckEscapeSatori(Object** dst, Object* ref);
-void CheckEscapeSatoriRange(void* dst, void* src, size_t len);
+void CheckEscapeSatoriRange(void* dst, size_t src, size_t len);
 
 //void SetCardsAfterBulkCopy(Object **start, size_t len);
 #endif // _GCHELPERS_H_
