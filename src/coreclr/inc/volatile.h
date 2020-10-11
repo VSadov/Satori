ba58e9e8a735b73baa03d12e5370fd4193f5490c
vsadov <vsadov@microsoft.com>
2020-10-10 23:24:38 -0700

    Generational GC works

--- src/coreclr/inc/volatile.h
+++ src/coreclr/inc/volatile.h
@@ -307,6 +307,27 @@ void VolatileLoadBarrier()
 }
 
 //
+// Memory ordering barrier that waits for stores in progress to complete.
+// Any effects of stores that appear before, in program order, will "happen before" relative to this.
+// Other operations such as computation, instruction prefetch or loads !!! are not guaranteed to be ordered.
+//
+// Architectural mapping:
+//   arm64  : dmb ishst
+//   arm    : dmb ish
+//   x86/64 : compiler fence
+inline
+void VolatileStoreBarrier()
+{
+#if defined(HOST_ARM64) && defined(__GNUC__)
+    asm volatile ("dmb ishst" : : : "memory");
+#elif defined(HOST_ARM64) && defined(_MSC_VER)
+    __dmb(_ARM64_BARRIER_ISHST);
+#else
+    VOLATILE_MEMORY_BARRIER();
+#endif   
+}
+
+//
 // Volatile<T> implements accesses with our volatile semantics over a variable of type T.
 // Wherever you would have used a "volatile Foo" or, equivalently, "Foo volatile", use Volatile<Foo>
 // instead.  If Foo is a pointer type, use VolatilePtr.
