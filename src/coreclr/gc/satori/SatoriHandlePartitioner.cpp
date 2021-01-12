9fc04a0147cc4b42f61e2744c04c48d1d4210cdf
vsadov <vsadov@microsoft.com>
2021-01-11 17:14:58 -0800

    Added SatoriHandlePartitioner

---
+++ src/coreclr/gc/satori/SatoriHandlePartitioner.cpp
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+//
+// SatoriHandlePartitioner.cpp
+//
+
+#include "common.h"
+
+#include "gcenv.h"
+#include "../env/gcenv.os.h"
+
+#include "SatoriHandlePartitioner.h"
+
+// static
+int SatoriHandlePartitioner::s_partitionCount;
+// static
+uint8_t* SatoriHandlePartitioner::s_scanTickets;
+//static
+uint8_t SatoriHandlePartitioner::s_currentTicket;
