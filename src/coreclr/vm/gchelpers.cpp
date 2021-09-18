ca1fa165e61a82466ea22d5c025c187ac7e144f1
vsadov <8218165+VSadov@users.noreply.github.com>
2021-09-17 20:14:59 -0700

    demotion gen2 -> gen1

--- src/coreclr/vm/gchelpers.cpp
+++ src/coreclr/vm/gchelpers.cpp
@@ -1478,7 +1478,7 @@ void ErectWriteBarrier(OBJECTREF *dst, OBJECTREF ref)
             return;
     }
 
-    page->DirtyCardForAddress((size_t)dst);
+    page->DirtyCardForAddressUnordered((size_t)dst);
 
 #else
     // if the dst is outside of the heap (unboxed value classes) then we
@@ -1599,7 +1599,7 @@ SetCardsAfterBulkCopy(Object** dst, Object **src, size_t len)
                     return;
             }
 
-            page->DirtyCardsForRange((size_t)dst, (size_t)dst + len);
+            page->DirtyCardsForRangeUnordered((size_t)dst, (size_t)dst + len);
         }
 
 #else
