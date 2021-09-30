c79290a4d221074c50b124dc6643db2f84db7a3e
vsadov <8218165+VSadov@users.noreply.github.com>
2021-09-30 12:16:38 -0700

    OSX x64

--- src/coreclr/vm/threads.h
+++ src/coreclr/vm/threads.h
@@ -6190,7 +6190,8 @@ inline BOOL IsWriteBarrierCopyEnabled()
 #ifdef DACCESS_COMPILE
     return FALSE;
 #else // DACCESS_COMPILE
-#ifdef HOST_OSX
+// TODO: Satori Barrier relocation is used only on OSX/ARM64
+#if defined(HOST_OSX) && defined(HOST_ARM64)
     return TRUE;
 #else
     return ExecutableAllocator::IsWXORXEnabled();
