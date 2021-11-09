806ca0f991cc8223433519c8900f0c7e598cf3f4
vsadov <8218165+VSadov@users.noreply.github.com>
2021-11-09 12:31:54 -0800

    GetTotalAllocatedBytes

--- src/libraries/System.Runtime/tests/System.Runtime.Tests/System/GCTests.cs
+++ src/libraries/System.Runtime/tests/System.Runtime.Tests/System/GCTests.cs
@@ -870,7 +870,6 @@ namespace System.Tests
             GC.CancelFullGCNotification();
         }
 
-        [ActiveIssue("Satori NYI")]
         [Theory]
         [InlineData(1000)]
         [InlineData(100000)]
@@ -955,7 +954,6 @@ namespace System.Tests
             }).Dispose();
         }
 
-        [ActiveIssue("Satori NYI")]
         [Fact]
         [ActiveIssue("https://github.com/dotnet/runtime/issues/42883", TestRuntimes.Mono)]
         public static void GetTotalAllocatedBytes()
