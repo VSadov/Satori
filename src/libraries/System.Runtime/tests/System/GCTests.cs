04777b9b812308c5edd44afb2f6f204180aee34f
vsadov <8218165+VSadov@users.noreply.github.com>
2021-11-14 10:42:22 -0800

    GC.GetTotalMemory

--- src/libraries/System.Runtime/tests/System/GCTests.cs
+++ src/libraries/System.Runtime/tests/System/GCTests.cs
@@ -364,7 +364,6 @@ namespace System.Tests
             }
         }
 
-        [ActiveIssue("Satori NYI")]
         [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsPreciseGcSupported))]
         public static void GetTotalMemoryTest_ForceCollection()
         {
