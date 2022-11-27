35f7f5f1826b1ca0a54485148a2a7427105fb4b5
vsadov <8218165+VSadov@users.noreply.github.com>
2022-11-26 19:38:02 -0800

    GCHeapStats ETW event

--- src/coreclr/gc/satori/SatoriRecycler.cpp
+++ src/coreclr/gc/satori/SatoriRecycler.cpp
@@ -737,6 +737,16 @@ bool SatoriRecycler::IsBlockingPhase()
 //        - could collect and use past history of the program behavior
 //        - could consider user input as to favor latency or throughput
 //        - ??
+
+// we target 1/EPH_SURV_TARGET ephemeral survival rate
+#define EPH_SURV_TARGET 4
+
+// we target 1/10 of total heap to be ephemeral. If more, we promote.
+#define EPH_RATIO 10
+
+// do gen2 when total doubles
+#define GEN2_THRESHOLD 2
+
 void SatoriRecycler::MaybeTriggerGC(gc_reason reason)
 {
     int generation = 0;
@@ -746,7 +756,10 @@ void SatoriRecycler::MaybeTriggerGC(gc_reason reason)
         generation = 1;
     }
 
-    if (m_gen1AddedSinceLastCollection + m_gen2AddedSinceLastCollection > m_totalBudget)
+    size_t currentAddedEstimate = m_gen2AddedSinceLastCollection +
+        m_gen1AddedSinceLastCollection / EPH_SURV_TARGET;
+
+    if (currentAddedEstimate > m_totalBudget)
     {
         generation = 2;
     }
@@ -775,16 +788,19 @@ size_t GetAvailableMemory()
 
 void SatoriRecycler::AdjustHeuristics()
 {
+    // ocupancies as of last collection
     size_t occupancy = GetTotalOccupancy();
     size_t ephemeralOccupancy = m_occupancy[1] + m_occupancy[0];
 
     if (m_prevCondemnedGeneration == 2)
     {
-        // we do gen2 if occupancy doubles, even if not using much memory
-        m_totalLimit = occupancy * 2;
+        m_totalLimit = occupancy * GEN2_THRESHOLD;
     }
 
-    size_t currentTotalEstimate = occupancy + m_gen1AddedSinceLastCollection + m_gen2AddedSinceLastCollection;
+    size_t currentTotalEstimate = occupancy +
+        m_gen2AddedSinceLastCollection +
+        m_gen1AddedSinceLastCollection / EPH_SURV_TARGET;
+
     m_totalBudget = m_totalLimit > currentTotalEstimate ?
         max(MIN_GEN1_BUDGET, m_totalLimit - currentTotalEstimate) :
         MIN_GEN1_BUDGET;
@@ -793,18 +809,16 @@ void SatoriRecycler::AdjustHeuristics()
     size_t available = GetAvailableMemory() * 9 / 10;
     m_totalBudget = min(m_totalBudget, available);
 
-    // if prev promoted, gen1 occupancy will be low, just keep the same budget.
-    // otherwise adjust
-    if (!m_promoteAllRegions)
-    {
-        // we look for ~20% ephemeral survivorship, also
-        // at least 1/8 total budget or gen1 min.
-        size_t minGen1 = max(MIN_GEN1_BUDGET, m_totalBudget / 8);
-        size_t newGen1Budget = max(minGen1, ephemeralOccupancy * 4);
+    // we look for 1 / EPH_SURV_TARGET ephemeral survivorship, thus budget is ephemeralOccupancy * EPH_SURV_TARGET
+    // we compute that based on actual ephemeralOccupancy or (occupancy / EPH_RATIO / 2), whichever is larger
+    // and limit that to MIN_GEN1_BUDGET
+    size_t minGen1 = max(MIN_GEN1_BUDGET, occupancy * EPH_SURV_TARGET / EPH_RATIO / 2);
 
-        // TUNING: using exponential smoothing with alpha == 1/2. is it a good smooth/lag balance?
-        m_gen1Budget = (m_gen1Budget + newGen1Budget) / 2;
-    }
+    size_t newGen1Budget = max(minGen1, ephemeralOccupancy * EPH_SURV_TARGET);
+
+    // smooth the budget a bit
+    // TUNING: using exponential smoothing with alpha == 1/2. is it a good smooth/lag balance?
+    m_gen1Budget = (m_gen1Budget + newGen1Budget) / 2;
 
     if (m_condemnedGeneration == 2)
     {
@@ -832,7 +846,7 @@ void SatoriRecycler::AdjustHeuristics()
         //    m_allowPromotingRelocations = true;
         //}
 
-        if (ephemeralOccupancy * 10 > occupancy)
+        if (ephemeralOccupancy * EPH_RATIO > occupancy)
         {
             m_promoteAllRegions = true;
         }
@@ -888,6 +902,18 @@ void SatoriRecycler::BlockingCollect()
     m_occupancyAcc[1] = 0;
 
     _ASSERTE(m_deferredSweepRegions->IsEmpty());
+
+    FIRE_EVENT(GCHeapStats_V2,
+        m_occupancy[0], 0,
+        m_occupancy[1], 0,
+        m_occupancy[2], 0,
+        0, 0,
+        0, 0,
+        0,
+        0,
+        0,
+        0,
+        0);
 
     // now we know survivorship after the last GC
     // and we can figure what we want to do in this GC and when we will do the next one
