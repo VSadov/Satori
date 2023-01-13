59d101099d0832a1bac33c640e281c16e250fc5f
vsadov <vsadov@live.com>
2023-01-12 20:08:10 -0800

    fix stuff up after rebase

--- src/coreclr/gc/unix/gcenv.unix.cpp
+++ src/coreclr/gc/unix/gcenv.unix.cpp
@@ -570,7 +570,7 @@ void* GCToOSInterface::VirtualReserve(void* location, size_t size)
 
     if (pRetVal != location)
     {
-        munmap(location, size);
+        munmap(pRetVal, size);
         return NULL;
     }
 
@@ -578,8 +578,8 @@ void* GCToOSInterface::VirtualReserve(void* location, size_t size)
         // Do not include reserved memory in coredump.
         madvise(pRetVal, size, MADV_DONTDUMP);
 #endif
-        return pRetVal;
-    }
+    return pRetVal;
+}
 
 // Release virtual memory range previously reserved using VirtualReserve
 // Parameters:
--- src/coreclr/vm/gchelpers.cpp
+++ src/coreclr/vm/gchelpers.cpp
@@ -960,6 +960,7 @@ OBJECTREF AllocateObject(MethodTable *pMT
 #ifdef FEATURE_COMINTEROP
                          , bool fHandleCom
 #endif
+                         , bool fUnmovable
     )
 {
     CONTRACTL {
@@ -1011,6 +1012,9 @@ OBJECTREF AllocateObject(MethodTable *pMT
         DWORD totalSize = pMT->GetBaseSize();
         if (totalSize >= g_pConfig->GetGCLOHThreshold())
             flags |= GC_ALLOC_LARGE_OBJECT_HEAP;
+
+        if (fUnmovable)
+            flags |= GC_ALLOC_PINNED_OBJECT_HEAP;
 
 #ifdef FEATURE_64BIT_ALIGNMENT
         if (pMT->RequiresAlign8())
