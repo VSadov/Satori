675ec9f80bbb392060ebe828e1c66302f6845abf
vsadov <vsadov@live.com>
2023-01-12 20:08:10 -0800

    fix stuff up after rebase

--- src/coreclr/gc/unix/gcenv.unix.cpp
+++ src/coreclr/gc/unix/gcenv.unix.cpp
@@ -581,7 +581,7 @@ void* GCToOSInterface::VirtualReserve(void* location, size_t size)
 
     if (pRetVal != location)
     {
-        munmap(location, size);
+        munmap(pRetVal, size);
         return NULL;
     }
 
@@ -589,8 +589,8 @@ void* GCToOSInterface::VirtualReserve(void* location, size_t size)
         // Do not include reserved memory in coredump.
         madvise(pRetVal, size, MADV_DONTDUMP);
 #endif
-        return pRetVal;
-    }
+    return pRetVal;
+}
 
 // Release virtual memory range previously reserved using VirtualReserve
 // Parameters:
