4625735f9000c344268b276758506be5d3310799
vsadov <8218165+VSadov@users.noreply.github.com>
2026-09-06 19:47:52 -0700

    barriers comments

--- src/coreclr/vm/amd64/patchedcode.asm
+++ src/coreclr/vm/amd64/patchedcode.asm
@@ -281,7 +281,7 @@ endif
         jb      RecordEscape            ; target is exposed. record an escape.
 
     JustAssign:
-        mov     [rcx], rdx              ; no card marking, src is not a heap object
+        mov     [rcx], rdx              ; src is external/null, or the assignment is completely threadlocal
         ret
 
     AssignAndMarkCards:
@@ -299,18 +299,18 @@ endif
         ret
 
     DoCards:
-    ; if same region, just check if barrier is not concurrent
+    ; if src and dst are in the same region, cards are not needed, unless concurrent
         xor     rdx, rcx
         shr     rdx, 21
-        jz      CheckConcurrent
+        jz      CheckConcurrent         ; same region, just check if barrier is not concurrent
 
-    ; if src is in gen2/3 and the barrier is not concurrent we do not need to mark cards
+    ; dst is in another region - cards are needed if src is ephemeral (gen < 2)
         cmp     dword ptr [r8 + 16], 2
         jl      MarkCards
 
     CheckConcurrent:
         cmp     r11, 0h
-        je      Exit
+        je      Exit                    ; if not concurrent, exit
 
     MarkCards:
     ; fetch card location for rcx
--- src/coreclr/vm/arm64/patchedcode.asm
+++ src/coreclr/vm/arm64/patchedcode.asm
@@ -518,7 +518,7 @@ exit$name
     WRITE_BARRIER_ENTRY JIT_CheckedWriteBarrier
     ; See if dst is in GCHeap
         ldr     x16, wbs_card_bundle_table
-        lsr     x17, x14, #30                       ; source page index
+        lsr     x17, x14, #30                       ; src page index
         ldrb    w12, [x16, x17]
         cbnz    x12, CheckedEntry
 
@@ -545,7 +545,7 @@ NotInHeap
 #ifdef FEATURE_SATORI_EXTERNAL_OBJECTS
         ldr     x16, wbs_card_bundle_table
 CheckedEntry
-        lsr     x17, x15, #30                   ; source page index
+        lsr     x17, x15, #30                   ; src page index
         ldrb    w12, [x16, x17]
         cbz     x12, JustAssign                 ; null or external (immutable) object
 #else
@@ -569,7 +569,7 @@ CheckedEntry
         lsr         x17, x17, x12
         tbnz        x17, #0, RecordEscape       ; target is exposed. record an escape.
 
-    ; UNORDERED! assignment of unescaped, null or external (immutable) object
+    ; UNORDERED! src is external/null, or the assignment is completely threadlocal
 JustAssign
         str  x15, [x14], #8
         ret  lr
@@ -589,12 +589,12 @@ ExitNoCards
         ret     lr
 
 DoCards
-    ; if same region, just check if barrier is not concurrent
+    ; if src and dst are in the same region, cards are not needed, unless concurrent
         and     x12, x14, #0xFFFFFFFFFFE00000   ; target aligned to region
         cmp     x12, x16
         beq     CheckConcurrent    ; same region, just check if barrier is not concurrent
 
-    ; if src is in gen2/3 and the barrier is not concurrent we do not need to mark cards
+    ; dst is in another region - cards are needed if src is ephemeral (gen < 2)
         ldr     w12, [x16, 16]                  ; source region + 16 -> generation
         tbz     x12, #1, MarkCards
 
